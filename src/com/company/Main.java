6d70f90758b6c0b096ea972954ad6e79c7ae3f3f
Izudin <hasic.izudin@hotmail.com>
2022-01-20 14:38:38 +0100

    zadaca

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a=5.85;
+        double area=6*a*a;
+        double volume= a*a*a;
+        System.out.println("area is:"+area);
+        System.out.println("volume is:"+volume);
+    }
+}
